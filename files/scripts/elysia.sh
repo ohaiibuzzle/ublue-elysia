782943c92a6323039227482eae6ac44a7164e745
OHaiiBuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2025-11-24 00:48:12 +0700

    fix: add elysiaos base files

---
+++ files/scripts/elysia.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+curl -fsSL https://raw.githubusercontent.com/spicetify/cli/main/install.sh | sh
+
+# Clone ElysiaOS
+cd /tmp
+git clone https://github.com/ElysiaOS/ElysiaOS.git ely
+cd ely
+
+# .config
+mkdir -p /etc/skel/
+cp -rv /tmp/ely/.cache /etc/skel/.cache
+cp -rv /tmp/ely/.config /etc/skel/.config
+cp -rv /tmp/ely/.floorp /etc/skel/.floorp
+cp -rv /tmp/ely/.bashrc /etc/skel/.bashrc
+cp -rv /tmp/ely/.zshrc /etc/skel/.zshrc
+
+cp -rv /tmp/ely/.icons/* /usr/share/icons/
+cp -rv /tmp/ely/fonts/* /usr/share/fonts/
+cp -rv /tmp/ely/.themes/* /usr/share/themes/
+
+mkdir -p /boot/grub/themes/ElysianRealm
+cp -rv /tmp/ely/GRUB-THEME/ElysianRealm/* /boot/grub/themes/ElysianRealm
+
+echo "GRUB_THEME=/boot/grub/themes/ElysianRealm/themes.txt" > /etc/default/grub
+
+cp -rv /tmp/ely/SDDM/* /usr/share/sddm/themes/
+
+
+sed -i 's/swww/awww/g' /tmp/ely/bin/wallpaper-switch.sh
+cp -rv /tmp/ely/bin/* /usr/local/bin/
+cp -v /tmp/ely/services/wallpaper-auto* /usr/lib/systemd/system/
+cp -rv /tmp/ely/plymouth/* /usr/share/plymouth/themes/
+
+rm -rf /tmp/ely
+
+plymouth-set-default-theme elysiaos-style2
+fc-cache -fsv
